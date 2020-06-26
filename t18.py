8a1a76a9419eae48b4838528c73ca30eceb7c7ee
pgmabv <alexey.vorovich@gmail.com>
2020-06-25 22:36:54 -0400

    # 2D array (Column totals, row totals, complete totals)

--- t18.py
+++ t18.py
@@ -1,4 +1,4 @@
-# 2D array (sum,min,max)
+# 2D array (Column totals, row totals, complete totals)
 
 x = [
     [-11, -12],
@@ -9,19 +9,28 @@ x = [
 nc = len(x[0])
 nr = len(x)
 #print(nc, nr)
-sum = 0
-max = x[0][0]
-min = x[0][0]
-ir = 0
-while ir < nr:
-    ic = 0
-    while ic < nc:
-        sum = sum+x[ir][ic]
-        if x[ir][ic] > max:
-            max = x[ir][ic]
-        if x[ir][ic] < min:
-            min = x[ir][ic]
-        ic = ic+1
-
-    ir = ir+1
-print(sum, max, min)
+TotSum = 0
+TotMax = x[0][0]
+TotMin = x[0][0]
+ic = 0
+while ic < nc:
+    print("Starting inside loop")
+    ColSum = 0
+    ColMax = x[0][ic]
+    ColMin = x[0][ic]
+    ir = 0
+    while ir < nr:
+        ColSum = ColSum+x[ir][ic]
+        if x[ir][ic] > ColMax:
+            ColMax = x[ir][ic]
+        if x[ir][ic] < ColMin:
+            ColMin = x[ir][ic]
+        ir = ir+1
+    print("ColSum", ColSum, "ColMin", ColMin, "max", max)
+    TotSum = TotSum+ColSum
+    if ColMax > TotMax:
+        TotMax = ColMax
+    if ColMin < TotMin:
+        TotMin = ColMin
+    ic = ic+1
+print("Total Sum", TotSum, "Final Max", TotMax, "Final Min", TotMin)
