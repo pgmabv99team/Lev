e0d477204df63b507804229c49a0d790fcabf8c2
pgmabv <alexey.vorovich@gmail.com>
2020-06-30 21:02:13 -0400

    max for even numbers

--- t21.py
+++ t21.py
@@ -1,51 +1,51 @@
 
 
-
 def MyFunction(p):
     pl = len(p)
-    i = 0
-    sum = 0
-    nneg = 0
-    # find the first positive
-    j = 0
-    while j < pl:
-        if p[j] < 0:
-            break
-        j = j+1
-    if j < pl:
-        mymax = p[j]
-    else:
-        mymax = "no luck......try again?"
 
+    sum = 0
+    neven = 0
+    if x[0] % 2 == 0:
+        mymax = x[0]
+        mymaxfound = True
+    else:
+        mymaxfound = False
     # main loop
+    i = 0
     while i < pl:
-        if p[i] < 0:
+        if p[i] % 2 == 0:
             # working on sum
             sum = sum+p[i]
-            nneg += 1
-            # working on min
-            if p[i] > mymax:
-                mymax = p[i]
+            neven += 1
+            if mymaxfound==True:
+                if p[i] > mymax:
+                    mymax = p[i] 
+            else:
+                mymax=p[i]
+
         i = i+1
 
-    if nneg > 0:
-        ave = sum/nneg
+    if neven > 0:
+        ave = sum/neven
     else:
         ave = "|-|OLY SHIT THIS TOOK A LONG TIME"
-    return ave,mymax
-    
-#Main Function
-x = [-4, -1, -2, -5, -3]
+    if mymaxfound==False:
+        mymaxfinal="No"
+    else:
+        mymaxfinal=mymax
+    return ave, mymaxfinal
+
+
+# Main Function
+x = [-3, -1]
 y = [233, -4, -45, -7865, 234]
 print(x)
-res=MyFunction(x)
-resave=res[0]
-resmax=res[1]
-print("for negatives:average=", resave, "maximum=",resmax)
-print(y)
-res=MyFunction(y)
-resave=res[0]
-resmax=res[1]
+res = MyFunction(x)
+resave = res[0]
+resmax = res[1]
 print("for negatives:average=", resave, "maximum=", resmax)
-
-
+print(y)
+res = MyFunction(y)
+resave = res[0]
+resmax = res[1]
+print("for negatives:average=", resave, "maximum=", resmax)
