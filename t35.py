84ddef0650c3bc5b109166a13f9ce2623ee0c640
pgmabv <alexey.vorovich@gmail.com>
2020-07-28 13:19:48 -0400

    hi

--- t35.py
+++ t35.py
@@ -1,5 +1,6 @@
 # Dictionary of tokenised words
 
+# find key in the dictionary
 import string
 def find_key(xDict, key):
     for word in xDict:
@@ -14,38 +15,53 @@ def make_word_list(p):
     iBWord = 0
     WordList = []
     for i in range(pl):
-        print(i, p[i])
+        # print(i, p[i])
         # if (p[i] == " ") or (p[i]=="\n") or (p[i]=="!") :
-        if (p[i] in punc) or (p[i]=="\n"):
+        if (p[i] in punc) or (p[i]=="\n") or (p[i]==" "):
             iSep = i
-            if len(p[iBWord:iSep]) > 0:
-                WordList.append(p[iBWord:iSep])
-            print("separator space at position", iSep, p[iBWord:iSep])
+            myslice=p[iBWord:iSep]
+            myslice2=myslice.lower()
+            if myslice2 !="the" and  myslice2 != "a" and len(myslice2)>0:
+                
+                WordList.append(myslice2)
+            # print("separator space at position", iSep, p[iBWord:iSep])
             iBWord = iSep+1
 
     return WordList
 
-# convert text string to array of tokens
-f = open("data_35.txt", "r")
+def find_mymax_value(p):
+    mymax=0
+    for key in p:
+        if p[key]>mymax:
+            mymax=p[key]
+            mykey=key
+
+    return mykey
+    
+# convert tekeyt string to array of tokens
+f = open("data_35a.txt", "r")
 data=f.read()+" "
-print(data)
-phrase = " even  ate  nine potato Lev BIGGER BADDER BETTER Lev Lev Epic LevLev "
+# print(data)
 words = make_word_list(data)
 print(words)
 
 # build a dictionary key=word, value=count
 xDict = {}
 for word in words:
-    found = find_key(xDict, word)
+    low_word=word.lower()
+    found = find_key(xDict,low_word)
     if found == True:
-        xDict[word] = xDict[word]+1
+        xDict[low_word] = xDict[low_word]+1
     else:
-        xDict[word] = 1
+
+        xDict[low_word] = 1
     # xDict[word] +=1
-    print(found)
+    # print(found)
 
 # print result
-print(xDict)
-
-
-print(punc)
+# print(xDict)
+dl=(len(xDict))
+for i in range(dl):
+    key_for_max=find_mymax_value(xDict)
+    print("found at ===",key_for_max,"value ",xDict[key_for_max])
+    xDict.pop(key_for_max)
