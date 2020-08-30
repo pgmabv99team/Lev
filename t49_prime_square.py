6b1cf3257f91c67105a18e8bdffd346d3bba39c2
pgmabv2 <pgmabv2@pgmabv12.ibi.com>
2020-08-29 21:16:00 -0400

    bits, tildes, and square work

--- t49_prime_square.py
+++ t49_prime_square.py
@@ -1,4 +1,5 @@
 import math
+import random
 def prime(p):
     pl=len(p)
     i=0
@@ -45,41 +46,69 @@ def prime2(p):
     print("Divided ",divide,"times")
 # prime2(37)
 
-def mysqrt(p):
-    i=0
+# def mysqrt(p):
+#     i=0
 
-    while i<p:
-        if i*i==p:
-            print("sqrt of",p,"is",i)
-            break
-        elif i*i>p:
-            print("The sqrt is between",i-1,"and",i)
-            break
-        else:
-            i +=1
+#     while i<p:
+#         if i*i==p:
+#             print("sqrt of",p,"is",i)
+#             break
+#         elif i*i>p:
+#             print("The sqrt is between",i-1,"and",i)
+#             break
+#         else:
+#             i +=1
 
-def sqrt_range(p):
-    i=0
-
-    while i<p:
-        if i*i>p:
-            
-            break
-        i +=1
-    return i-1,i
-p=10
-res=sqrt_range(p)
-low=res[0]
-high=res[1]
-for i in range(16):
-    mid=(low+high)/2
-    if mid**2>p:
-        print("sqrt between",low,"and",mid)
-        high=mid
-
+def mysqrt2(p):
+    low=0
+    if p>=1:
+        high=p/2
     else:
-        print("sqrt between",mid,"and",high)
-        low=mid
-print(math.sqrt(p))
+        high=p
+    for i in range(16):
+        mid=(low+high)/2
+        if mid**2>p:
+            print("sqrt between",low,"and",mid)
+            high=mid
+
+        else:
+            print("sqrt between",mid,"and",high)
+            low=mid
+    return low
+num=6
+# mysqrt2(27)
+def make_random(p):
+    i=0
+    while i<num:
+        p.append(random.randint(0,100))
+        i +=1
+    
+x=[]
+make_random(x)
+
+i=0
+xl=len(x)
+q=[]
+while i<xl:
+    res=mysqrt2(x[i])
+    q.append(res)
+    i +=1
+print(x)
+print(q)
+
+def turn_around(p):
+    print(p)
+    i=0
+    pl=len(p)
+    pl2=len(p)
+    while i<pl/2:
+        a=p[i]
+        p[i]=p[pl-i-1]
+        p[pl-i-1]=a
+        i +=1
+        
+    print(p)
 
 
+turn_around(x)
+turn_around(q)
