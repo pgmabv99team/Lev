30caa01337ae02c9b1f7005d44401bc32e6a9764
pgmabv2 <pgmabv2@pgmabv12.ibi.com>
2020-09-08 13:16:15 -0400

    calc string

--- t55_paren.py
+++ t55_paren.py
@@ -2,7 +2,8 @@
 # print(x)
 y="())(()"
 # y="()())(()"
-x=")("
+x="(())()"
+
 # for i in range(len(y)):
 #     print(y[i])
 
@@ -48,10 +49,36 @@ def paren_stack(p):
                 stk.pop()  
             
 
-        # print("after",stk)    
+        print("after",stk)    
         i +=1
     if len(stk)!=0:
         print("unbalanced left paren at position",i)
         print(p)
         return False
-print(paren_stack(x))
+# print(paren_stack(x))
+
+z="1+24+34-56"
+def calc(p):
+    pl=len(p)
+    i=1
+    mysum=int(p[0])
+    # print(mysum)
+    while i<pl-1:
+        j=i+1
+        while j<pl:
+            if p[j]=="+" or p[j]=="-":
+                break
+            
+            j +=1
+        t=p[i+1:j]
+        print("t",t)
+        print("operator",p[i])
+        
+        if p[i]=="+":
+            mysum=mysum+int(t)
+        elif p[i]=="-":
+            mysum=mysum-int(t)
+        print(p[i],t,mysum)
+        i=j
+    print(mysum)
+calc(z)
