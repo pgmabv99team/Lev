4dcb6993fddc16ad1eb1e21e8c841ed3ff4fb85c
Lev Vorovich <lev.vorovich2@gmail.com>
2020-11-19 22:07:28 -0500

    permitations

--- t108dict_in_dict.py
+++ t108dict_in_dict.py
@@ -2,8 +2,10 @@ import json
 p="pow wow cow cow birb BiRb pOTtATo potato "
 d={}
 words=p.split()
+i=0
 for word in words:
     word2=word.lower()
+    
     if word2 not in d:
         urlg="https://www.google.com/search?q="+word2
         urlw="https://en.wikipedia.org/wiki/"+word2
@@ -13,9 +15,16 @@ for word in words:
         tmpd["urlw"]=urlw
         tmpd["urlb"]=urlb
         tmpd["count"]=1
+        poss=[]
+        poss.append(i)
+        tmpd["kposs"]=poss
         d[word2]=tmpd
     else:
         d[word2]["count"] +=1
+        poss=d[word2]["kposs"]
+        poss.append(i)
+        d[word2]["kposs"]=poss
+    i +=1
      
 print(d)
 for key in d:
--- t109paren2.py
+++ t109paren2.py
@@ -22,12 +22,49 @@ def stk_paren(p):
         return False
 
 
-p="((()))"
-print(stk_paren(p))
-p="("
-print(stk_paren(p))
-p="(()"
-print(stk_paren(p))
-p="()())"
-print(stk_paren(p))
+
+
+
+
+def stk_paren2(p):
+    pl=len(p)
+    i=0
+    stk=[]
+    if len(p)==0 or p[pl-1]=="(" or p[pl-1]=="[":
+        print("unpaired left paren at the end of the string")
+        return False
+    while i<pl:
+        top=len(stk)-1
+        if p[i] in ("(","["):
+            stk.append(p[i])
+        elif len(stk)==0 and p[i] in (")","]"):
+            print("Right paren with no partner. Position:",i,"=",p[i:])
+            return False
+        elif len(stk)>0 :
+            if stk[top]=="(" and p[i]==")":
+                stk.pop()  
+            elif stk[top]=="[" and p[i]=="]":
+                stk.pop()   
+            else:
+                print("stack",stk,"next",i,"in position",p)
+                return False  
+
+        i +=1
+    if stk==[]:
+        return True
+    else:
+        print("False because not empty stack; too many left parens",stk)
+        return False
+
+
+# p="(([]))"
+# print(stk_paren2(p))
+p="()(([([])))]"
+print(stk_paren2(p))
+# p="("
+# print(stk_paren(p))
+# p="(()"
+# print(stk_paren(p))
+# p="()())"
+# print(stk_paren(p))
 
