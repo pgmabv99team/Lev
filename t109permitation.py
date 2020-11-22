e2f395f0d67c1c6a0f1fee6609e611c6344de6c5
Lev Vorovich <lev.vorovich2@gmail.com>
2020-11-22 13:29:11 -0500

    permitation with recursion

--- t109permitation.py
+++ t109permitation.py
@@ -11,15 +11,64 @@ def perm(p):
 
         i0 +=1
 
+
+def is_unique(nums):
+    nl=len(nums)
+    x={}
+    for num in range(nl):
+        mykey=nums[num]
+        if mykey in x:
+            return False
+        else:
+            x[mykey]=nums[num]
+    return True
+
+
 x=['a','b','c','d']
-def perm2(p):
+def perm2(p,m):
     pl=len(p)
     cnt=0
+    ids=[0]*m
     for i0 in range(pl):
+        ids[0]=i0
         for i1 in range(pl):
+            ids[1]=i1
             for i2 in range(pl):
-                if i0!=i1 and i1!=i2 and i2!=i0:
+                ids[2]=i2
+                print(ids)
+                if is_unique(ids):
                     cnt +=1
                     print(p[i0],p[i1],p[i2])
     print(cnt)
-perm2(x) 
+# permitation with recursion
+def perm2_rec(p,m,ids,depth):
+    pl=len(p)
+    global cnt
+    for i0 in range(pl):
+        ids[depth]=i0
+        if depth==m-1:
+            # full depth reached
+            if is_unique(ids):
+                # ids are unique
+                cnt +=1
+                print(ids)
+                for id in ids:
+                    print(p[id],end=" ")
+                print("\n")
+        else:
+            # depth not reached. call func again
+            perm2_rec(p,m,ids,depth+1)
+
+x=['a','b','c','d','e']
+m=3
+ids=[0]*m
+depth=0    
+global cnt
+cnt=0
+perm2_rec(x,m,ids,depth) 
+print(cnt)
+
+
+# arr=[1]
+# print(is_unique(arr))
+
