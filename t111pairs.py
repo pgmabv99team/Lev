17f40c4ea951a80c6e859cbd14ae3ccb2c259a18
Lev Vorovich <lev.vorovich2@gmail.com>
2020-12-04 20:35:28 -0500

    dict way for pairs

--- t111pairs.py
+++ t111pairs.py
@@ -19,24 +19,33 @@ def pairs(p):
         if mybreak==False:
             i +=1
     return tmp[0]
-x=[2,2,1,2,2]
-print(x,pairs(x))
+# x=[2,2,1,2,2]
+# print(x,pairs(x))
 
-x=[2,1,2]
-print(x,pairs(x))
+# x=[2,1,2]
+# print(x,pairs(x))
 
-x=[3,3,4,4,1]
-print(x,pairs(x))
+# x=[3,3,4,4,1]
+# print(x,pairs(x))
 
-x=[2,2,9,2,2]
-print(x,pairs(x))
+# x=[2,2,9,2,2]
+# print(x,pairs(x))
 
-x=[2,2,2,2,0]
-print(x,pairs(x))
+# x=[2,2,2,2,0]
+# print(x,pairs(x))
 
-x=[11,2,2,2,2]
-print(x,pairs(x))
+# x=[11,2,2,2,2]
+# print(x,pairs(x))
 
-#av change 3
-# lv change 200
-#av change 4
+def pairs2(p):
+    d={}
+    for el in p:
+        if el in d:
+            d[el] +=1
+        else:
+            d[el]=1
+
+    for key in d:
+        if d[key]%2!=0:
+            return key
+print(pairs2([1,1,3,3,4]))
