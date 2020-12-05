39eb49286b09687c58bf0f85f01927e974207fd3
alexey vorovich <alexey.vorovich.gmail.com>
2020-12-04 19:27:00 -0500

    av  change3

--- t111pairs.py
+++ t111pairs.py
@@ -28,13 +28,13 @@ print(x,pairs(x))
 x=[3,3,4,4,1]
 print(x,pairs(x))
 
-x=[2,2,7,2,2]
+x=[2,2,9,2,2]
 print(x,pairs(x))
 
-x=[2,2,2,2,1]
+x=[2,2,2,2,0]
 print(x,pairs(x))
 
 x=[1,2,2,2,2]
 print(x,pairs(x))
 
-#av change 1
+#av change 3
