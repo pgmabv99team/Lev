5e929db80cf269a2a9fd5341d3a4075ee7c97a30
alexey vorovich <alexey.vorovich.gmail.com>
2020-12-04 19:15:39 -0500

    av change 1

--- t111pairs.py
+++ t111pairs.py
@@ -28,7 +28,7 @@ print(x,pairs(x))
 x=[3,3,4,4,1]
 print(x,pairs(x))
 
-x=[2,2,5,2,2]
+x=[2,2,7,2,2]
 print(x,pairs(x))
 
 x=[2,2,2,2,1]
@@ -37,4 +37,4 @@ print(x,pairs(x))
 x=[1,2,2,2,2]
 print(x,pairs(x))
 
-y=8
+#av change 1
