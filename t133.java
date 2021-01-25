5920c4ee9be27d80c63c38f760794fd34873b854
grinchybirb <lev.vorovich2@gmail.com>
2021-01-24 19:49:04 -0500

    jabba

---
+++ t133.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class t133 {
+    public static void main(String[] kwargs) {
+        System.out.println("Hello, World!");
+        var potato_list = new ArrayList<>();
+        for (var i = 0; i < 10; i++) {
+            potato_list.add(i);
+
+        }
+        System.out.println(potato_list);
+    }
+
+}
